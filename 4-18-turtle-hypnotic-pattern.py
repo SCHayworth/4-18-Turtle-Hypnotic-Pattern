f0fa7c48a1f18c53092ce3d2061803b06a6e97c0
Shaun Hayworth <thismoderndeath@gmail.com>
2019-10-24 13:50:25 -0700

    First draft of program code

---
+++ 4-18-turtle-hypnotic-pattern.py
@@ -0,0 +1,29 @@
+# Program 4-18 Turtle: Hypnotic Pattern
+# Shaun Hayworth
+# CIS 2
+# 10-24-2019
+# Original source code and revision history can be found at https://github.com/SCHayworth/4-18-Turtle-Hypnotic-Pattern
+
+# Draws a rectangular spiral pattern using the turtle graphics library
+
+# Import the turtle graphics library
+import turtle
+
+# Initialize ITERATIONS constant at 30
+# Sets the mumber of times the loop should execute.
+ITERATIONS = 30
+
+# Set the length of the first line to 5 pixels
+line_length = 5
+
+# Set the initial direction of the turtle to up
+turtle.left(90)
+
+# Draw the pattern using a loop
+for x in range(ITERATIONS):
+    turtle.forward(line_length)     # Draws a line equal to the line_length value
+    turtle.left(90)                 # Rotates the turtle left 90 degrees
+    line_length *= 2                # Doubles the length of the next line
+
+# Keeps the turtle display window open if program is not run in IDLE
+turtle.done()
